2ec366177d4956423ed514900d3ead5425648cbb
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2025-12-19 11:47:42 +0100

    feat: add favicon

--- ai/app.py
+++ ai/app.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from pathlib import Path
 
 app = FastAPI()
+
+BASE_DIR = Path(__file__).resolve().parent
+FAVICON_PATH = BASE_DIR / "static" / "favicon.ico"
+
+@app.get("/favicon.ico", include_in_schema=False)
+def favicon():
+    return FileResponse(FAVICON_PATH)
 
 @app.get("/")
 def read_root():
     return {"message": "Hello depuis ai 🚀"}
+
